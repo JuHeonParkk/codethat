72c77b9f589fb4ad938d68e5930c0f5a6e71c9c3
JuHeonParkk <ju10913@naver.com>
2026-02-13 15:24:22 +0900

    refactor: 파일 확장자 jsx로 변경

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./components/App.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import CourseListPage from "./pages/CourseListPage.jsx";
+import CoursePage from "./pages/CoursePage.jsx";
+import WishlistPage from "./pages/WishlistPage.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <App>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="courses" element={<CourseListPage />} />
+        <Route
+          path="courses/react-frontend-development"
+          element={<CoursePage />}
+        />
+        <Route path="wishlist" element={<WishlistPage />} />
+      </Routes>
+    </App>
   </BrowserRouter>,
 );
